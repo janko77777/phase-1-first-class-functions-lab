8ebef13c45e030ff392d3f4d7531a04a0f48e1f6
Steven Jankowski <steven.jankowski1@gmail.com>
2021-12-17 23:24:09 +0000

    commit tree 47688464921cc5cf6b47c786fb333fbd81cebf48

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+
+const array = ['Antonia', 'Nuru', 'Amari', 'Claudia']
+
+const returnFirstTwoDrivers = function(array){
+    let newArray = [...array].slice(0, 2)
+    return newArray
+}
+
+console.log(returnFirstTwoDrivers(array))
+
+const returnLastTwoDrivers = function(array){
+    let newArray2 = [...array].slice(array.length - 2, array.length)
+    return newArray2
+}
+
+const selectingDrivers = returnFirstTwoDrivers(array).concat(returnLastTwoDrivers(array))
+
+const createFareMultiplier = function(multiplier) {
+    return function(fare) {return fare*multiplier}
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+
+function selectDifferentDrivers(array, foo) {
+    return foo(array)
+}
